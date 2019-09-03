e8aa2c8c50d12f1b376343e57b2bf0ff6387389f
Naveed Hussain <49544857+nhussain2@users.noreply.github.com>
2019-09-02 22:17:16 -0500

    - added navigation bar, modified - space Navigation bar

--- app/app/src/main/java/com/example/app/MainActivity.java
+++ app/app/src/main/java/com/example/app/MainActivity.java
@@ -3,12 +3,49 @@ package com.example.app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.luseen.spacenavigation.SpaceItem;
+import com.luseen.spacenavigation.SpaceNavigationView;
+import com.luseen.spacenavigation.SpaceOnClickListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    SpaceNavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //add navigation view
+
+        navigationView = findViewById(R.id.space);
+
+        navigationView.initWithSaveInstanceState(savedInstanceState);
+        navigationView.addSpaceItem(new SpaceItem("", R.drawable.ic_home_black_24dp));
+        navigationView.addSpaceItem(new SpaceItem("", R.drawable.ic_add_box_black_24dp));
+        navigationView.addSpaceItem(new SpaceItem("", R.drawable.ic_history_black_24dp));
+        navigationView.addSpaceItem(new SpaceItem("", R.drawable.ic_favorite_black_24dp));
+
+        //set on click listeners for navigation view
+        navigationView.setSpaceOnClickListener(new SpaceOnClickListener() {
+            @Override
+            public void onCentreButtonClick() {
+                navigationView.setCentreButtonSelectable(true);
+
+            }
+
+            @Override
+            public void onItemClick(int itemIndex, String itemName) {
+                Toast.makeText(MainActivity.this, itemIndex + " " + itemName, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onItemReselected(int itemIndex, String itemName) {
+                Toast.makeText(MainActivity.this, itemIndex + " " + itemName, Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
