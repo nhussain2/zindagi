aae079458a3cfe5bc030e2a2d74e3000779ed898
Naveed Hussain <49544857+nhussain2@users.noreply.github.com>
2019-09-05 02:05:05 -0500

    -add fragments, finish bottom nav, populate frags w/ base texts

--- app/app/src/main/java/com/example/app/MainActivity.java
+++ app/app/src/main/java/com/example/app/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.app;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,52 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigationView= findViewById(R.id.bta_nav);
+
+        navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                int id= menuItem.getItemId();
+
+                if (id == R.id.home) {
+                    //creating home fragment
+                    HomeFragment fragment = new HomeFragment();
+                    FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction.replace(R.id.frame_layout, fragment);
+                    fragmentTransaction.commit();
+
+                }if (id == R.id.call) {
+                    //creating call fragment
+                    CallFragment fragment = new CallFragment();
+                    FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction.replace(R.id.frame_layout, fragment);
+                    fragmentTransaction.commit();
+
+                }if (id == R.id.personal) {
+                    //creating personal fragment
+                    PersonalFragment fragment = new PersonalFragment();
+                    FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction.replace(R.id.frame_layout, fragment);
+                    fragmentTransaction.commit();
+
+                }if (id == R.id.advice) {
+                    //creating advice fragment
+                    AdviceFragment fragment = new AdviceFragment();
+                    FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction.replace(R.id.frame_layout, fragment);
+                    fragmentTransaction.commit();
+                }
+
+
+
+
+
+                return true;
+            }
+        });
+        // set default to home
+        navigationView.setSelectedItemId(R.id.home);
     }
 }
