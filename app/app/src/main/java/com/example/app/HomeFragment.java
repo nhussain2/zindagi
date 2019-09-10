e459ff42318fc0202370c84dd50c12d7b45d19cd
Naveed Hussain <49544857+nhussain2@users.noreply.github.com>
2019-09-10 18:20:47 -0500

    add button to link to web github page

--- app/app/src/main/java/com/example/app/HomeFragment.java
+++ app/app/src/main/java/com/example/app/HomeFragment.java
@@ -1,6 +1,8 @@
 package com.example.app;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -14,7 +16,7 @@ import android.widget.Button;
  */
 public class HomeFragment extends Fragment {
 
-
+    Button open_git;
     public HomeFragment() {
         // Required empty public constructor
     }
@@ -26,10 +28,17 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-
-
         View v = inflater.inflate(R.layout.fragment_home, container, false);
+
+        open_git = (Button)v.findViewById(R.id.opengitbtn);
+        open_git.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent opengitIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/nhussain2/zindagi"));
+                startActivity(opengitIntent);
+            }
+        });
+
         return v;
     }
-
 }
